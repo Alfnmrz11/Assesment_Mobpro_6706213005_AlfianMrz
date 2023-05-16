91ca5fa53fb60ddde7eb6d978359d0221b9a1e60
alfnmrz11 <alfianmrizki123@gmail.com>
2023-05-17 03:28:16 +0700

    Penambahan Fitur dan Melakukan Perombakan Source Code part2

---
+++ app/src/main/java/org/d3if3005/deliveryfood/AboutActivity.kt
@@ -0,0 +1,13 @@
+package org.d3if3005.deliveryfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+    }
+}
