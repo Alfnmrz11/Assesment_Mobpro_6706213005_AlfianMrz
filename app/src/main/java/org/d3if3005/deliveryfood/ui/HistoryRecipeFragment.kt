4c5717fd06039c3a4c5710dfd31e1cbef045b3c5
alfnmrz11 <alfianmrizki123@gmail.com>
2023-05-20 15:40:22 +0700

    Penambahan HISTORY ADAPTER 1

--- app/src/main/java/org/d3if3005/deliveryfood/ui/HistoryRecipeFragment.kt
+++ app/src/main/java/org/d3if3005/deliveryfood/ui/HistoryRecipeFragment.kt
@@ -6,9 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import org.d3if3005.deliveryfood.db.RecipeDb
+import org.d3if3005.deliveryfood.ui.factory.HistoryViewModelFactory
+import org.d3if3005.deliveryfood.views.HistoryViewModel
 
 class HistoryRecipeFragment : Fragment() {
     private lateinit var binding: HistoryRecipeFragmentBinding
+
+    private val viewModel: HistoryViewModel by lazy {
+        val db = RecipeDb.getInstance(requireContext())
+        val factory = HistoryViewModelFactory(db.dao)
+        ViewModelProvider(this)[HistoryViewModel::class.java]
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle
     ): View? {
