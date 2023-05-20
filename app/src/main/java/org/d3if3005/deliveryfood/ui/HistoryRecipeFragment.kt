cf41f1d08c0db5468b0987356c75eb7de3dc2802
alfnmrz11 <alfianmrizki123@gmail.com>
2023-05-20 19:38:09 +0700

    Melakukan Penambahan Room dan View model 2

--- app/src/main/java/org/d3if3005/deliveryfood/ui/HistoryRecipeFragment.kt
+++ app/src/main/java/org/d3if3005/deliveryfood/ui/HistoryRecipeFragment.kt
@@ -11,25 +11,25 @@ import org.d3if3005.deliveryfood.db.RecipeDb
 import org.d3if3005.deliveryfood.ui.factory.HistoryViewModelFactory
 import org.d3if3005.deliveryfood.views.HistoryViewModel
 
-class HistoryRecipeFragment : Fragment() {
-    private lateinit var binding: HistoryRecipeFragmentBinding
-
-    private val viewModel: HistoryViewModel by lazy {
-        val db = RecipeDb.getInstance(requireContext())
-        val factory = HistoryViewModelFactory(db.dao)
-        ViewModelProvider(this)[HistoryViewModel::class.java]
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle
-    ): View? {
-        binding = HistoryRecipeFragmentBinding.inflate(layoutInflater,
-            container, false)
-        return binding.root
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.data.observe(viewLifecycleOwner, {
-            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
-        })
-    }
-}
+//class HistoryRecipeFragment : Fragment() {
+//    private lateinit var binding: HistoryRecipeFragmentBinding
+//
+//    private val viewModel: HistoryViewModel by lazy {
+//        val db = RecipeDb.getInstance(requireContext())
+//        val factory = HistoryViewModelFactory(db.dao)
+//        ViewModelProvider(this)[HistoryViewModel::class.java]
+//    }
+//
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+//                              savedInstanceState: Bundle
+//    ): View? {
+//        binding = HistoryRecipeFragmentBinding.inflate(layoutInflater,
+//            container, false)
+//        return binding.root
+//    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        viewModel.data.observe(viewLifecycleOwner, {
+//            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
+//        })
+//    }
+//}
