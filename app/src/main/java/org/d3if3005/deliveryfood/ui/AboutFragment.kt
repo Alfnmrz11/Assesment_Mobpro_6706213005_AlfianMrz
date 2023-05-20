43c3992d9ee3e3ff3750fb8062f0781843e81555
alfnmrz11 <alfianmrizki123@gmail.com>
2023-05-20 12:39:10 +0700

    Menyambungkan Fragment pt3

--- app/src/main/java/org/d3if3005/deliveryfood/ui/AboutFragment.kt
+++ app/src/main/java/org/d3if3005/deliveryfood/ui/AboutFragment.kt
@@ -3,22 +3,23 @@ package org.d3if3005.deliveryfood.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.d3if3005.deliveryfood.R
 import org.d3if3005.deliveryfood.databinding.FragmentAboutBinding
 
 
-class AboutFragment : Fragment() {
+class AboutFragment : AppCompatActivity() {
     private lateinit var binding: FragmentAboutBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    override fun onCreate(savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        // Perform any additional setup or logic here
     }
 }
--- app/src/main/java/org/d3if3005/deliveryfood/ui/NewRecipeFragment.kt
+++ app/src/main/java/org/d3if3005/deliveryfood/ui/NewRecipeFragment.kt
@@ -3,23 +3,19 @@ package org.d3if3005.deliveryfood.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.d3if3005.deliveryfood.R
 import org.d3if3005.deliveryfood.databinding.FragmentAboutBinding
 import org.d3if3005.deliveryfood.databinding.FragmentNewRecipeBinding
 
 
-class NewRecipeFragment : Fragment() {
-    private lateinit var binding: FragmentNewRecipeBinding
+class NewRecipeFragment : AppCompatActivity() {
+    private lateinit var binding: FragmentAboutBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentNewRecipeBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
